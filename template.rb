2563c45d66f47531d95edbbe225717ddc66c2a77
surabhilabs <dheeraj@surabhilabs.in>
2015-03-23 23:05:58 +0530

    Update template.rb

--- template.rb
+++ template.rb
@@ -65,7 +65,7 @@ run "rake db:migrate"
 
 
 
-
+copy_from_repo '.rubocop.yml'
 
 # Bootstrap: install from https://github.com/twbs/bootstrap
 # Note: This is 3.0.0
@@ -80,6 +80,16 @@ if yes?("setup bootstrap?")
   run "rails generate  layout:devise foundation5"
 end
 run "rails g cancan:ability"
+
+# Setting up foreman to deal with environment variables and services
+# https://github.com/ddollar/foreman
+# ==================================================
+# Use Procfile for foreman
+run "echo 'web: bundle exec rails server -p $PORT' >> Procfile"
+run "echo PORT=3000 >> .env"
+run "echo '.env' >> .gitignore"
+# We need this with foreman to see log output immediately
+run "echo 'STDOUT.sync = true' >> config/environments/development.rb"
 
 
 # Ignore rails doc files, Vim/Emacs swap files, .DS_Store, and more
@@ -118,4 +128,40 @@ if yes?("Initialize GitHub repository?")
     git remote: %Q{ add origin git@github.com:#{username}/#{app_name}.git }
     git push: %Q{ origin master }
   end
+end
+
+
+def copy_from_repo(filename, opts = {})
+repo = 'https://raw.github.com/RailsApps/rails-composer/master/files/'
+repo = opts[:repo] unless opts[:repo].nil?
+if (!opts[:prefs].nil?) && (!prefs.has_value? opts[:prefs])
+return
+end
+source_filename = filename
+destination_filename = filename
+unless opts[:prefs].nil?
+if filename.include? opts[:prefs]
+destination_filename = filename.gsub(/\-#{opts[:prefs]}/, '')
+end
+end
+if (prefer :templates, 'haml') && (filename.include? 'views')
+remove_file destination_filename
+destination_filename = destination_filename.gsub(/.erb/, '.haml')
+end
+if (prefer :templates, 'slim') && (filename.include? 'views')
+remove_file destination_filename
+destination_filename = destination_filename.gsub(/.erb/, '.slim')
+end
+begin
+remove_file destination_filename
+if (prefer :templates, 'haml') && (filename.include? 'views')
+create_file destination_filename, html_to_haml(repo + source_filename)
+elsif (prefer :templates, 'slim') && (filename.include? 'views')
+create_file destination_filename, html_to_slim(repo + source_filename)
+else
+get repo + source_filename, destination_filename
+end
+rescue OpenURI::HTTPError
+say_wizard "Unable to obtain #{source_filename} from the repo #{repo}"
+end
 end
